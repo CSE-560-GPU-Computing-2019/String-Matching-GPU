a6814594bd17357f24942ab83b22ae3d3bb29893
Piyush Gupta <piyush16066@iiitd.ac.in>
2019-04-12 22:47:13 +0530

    Add test script

---
+++ test.sh
@@ -0,0 +1,10 @@
+./shiftor data/t_s.txt data/p_sample.txt
+echo
+./shiftor data/t_m.txt data/p_sample.txt
+echo
+./shiftor data/t_l.txt data/p_sample.txt
+echo
+./shiftor data/t_vl.txt data/p_sample.txt
+echo
+./shiftor data/t_vvl.txt data/p_sample.txt
+echo
